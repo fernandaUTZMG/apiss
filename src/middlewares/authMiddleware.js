540e1778977f839ed4fbee189595e2f7de77e7ad
fernandaUTZMG <fernanda.hernandez.22s@utzmg.edu.mx>
2025-02-15 16:02:19 -0600

    c

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken'); // Asegúrate de tener instalado jsonwebtoken
+
+// Middleware para verificar el token JWT
+module.exports = (req, res, next) => {
+    // Obtén el token del encabezado 'Authorization' (generalmente como Bearer token)
+    const token = req.header('Authorization')?.replace('Bearer ', ''); // Extrae el token si está presente
+
+    if (!token) {
+        return res.status(401).json({ message: 'Acceso denegado. No se proporcionó un token.' });
+    }
+
+    try {
+        // Verifica el token con la clave secreta
+        const decoded = jwt.verify(token, 'token'); // 'token' es tu clave secreta
+        // Si la verificación es exitosa, agregamos los datos decodificados al objeto req
+        req.usuario = decoded;
+        next(); // Llama al siguiente middleware o controlador
+    } catch (error) {
+        return res.status(400).json({ message: 'Token no válido o expirado.' });
+    }
+};
